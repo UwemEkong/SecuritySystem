1e0cab5d1d77944f1ae6fabf4cecd283d3509aee
GGILL <ggill@cosmodigital.com>
2022-02-10 16:25:13 -0600

    Can view flask stream from angular
    To run this, open terminal/cmd prompt
    From SecuritySystem cd into flask
    run: python3 stream.py
    (dependencies OpenCV, Flask)
    Then run angular as normal, and view stream from camera page

--- frontend/website/src/app/components/camera/camera.component.ts
+++ frontend/website/src/app/components/camera/camera.component.ts
@@ -11,19 +11,25 @@ export class CameraComponent implements OnInit {
 
   videoRef:any;
   ngOnInit(): void {
-    this.videoRef = document.getElementById('video');
-    console.log(this.videoRef);
-    this.setupCamera();
+    // this.videoRef = document.getElementById('video');
+    // console.log(this.videoRef);
+    // this.setupCamera();
   }
 
   setupCamera(){
-    navigator.mediaDevices.getUserMedia({
-      video:{width:500, height:500},
-      audio:false
-    }).then(stram =>{
-      console.log(stram);
-      this.videoRef.srcObject = stram;
-    });
+    //navigator.mediaDevices.getUserMedia({
+      //video:{width:500, height:500},
+      //audio:false
+    //}).then(stram =>{
+      //console.log(stram);
+      //this.videoRef.srcObject = stram;
+      //var video = document.querySelector('video');
+      //this.videoRef.src = "http://127.0.0.1:5000/video_feed";
+      // @ts-ignore
+      //video.src = 'http://127.0.0.1:5000/video_feed?'; // just to be sure src does not conflict with us
+      // @ts-ignore
+      //video.srcObject = localMediaStream;
+    // });
   }
 
 }
