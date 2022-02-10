3175dd2a7f9b0155f4bf86ff86239bf1f17c0777
AdamChaplin23 <b2482993@ben.edu>
2022-02-10 14:25:59 -0600

    date shite

--- flask/stream.py
+++ flask/stream.py
@@ -2,14 +2,29 @@
 #Import necessary libraries
 from flask import Flask, render_template, Response
 import cv2
+import datetime
 #Initialize the Flask app
 app = Flask(__name__)
 
 camera = cv2.VideoCapture(0)
 
 def gen_frames():
-    while True:
+    while camera.isOpened():
         success, frame = camera.read()
+        if success:
+       
+            font = cv2.FONT_HERSHEY_PLAIN
+ 
+            #date and time as variable
+            dt = str(datetime.datetime.now())
+ 
+        
+            #lay date and time video frame
+            frame = cv2.putText(frame, dt,
+                            (10, 100),
+                            font, 1,
+                            (210, 155, 155),
+                            4, cv2.LINE_8)
         if not success:
             break
         else:
@@ -17,6 +32,7 @@ def gen_frames():
             frame = buffer.tobytes()
             yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
+                   
 
 
 @app.route('/')
