8835e4f413aaba877f8779091ce399e6c6168682
GGILL <ggill@cosmodigital.com>
2022-05-04 16:20:02 -0500

    style fixes

--- mobile/lib/widgets/RecordTile.dart
+++ mobile/lib/widgets/RecordTile.dart
@@ -15,31 +15,39 @@ class _RecordTileState extends State<RecordTile> {
   Widget build(BuildContext context) {
     print(widget.mediaxObject);
     print("BIGBROOOOOOO");
-    return ListTile(
-      tileColor: Colors.white,
-      title: Text(widget.mediaxObject.filename,
-          style: TextStyle(
-              fontFamily: "Trajan Pro",
-              height: 1.0,
-              fontSize: 10,
-              fontWeight: FontWeight.bold,
-              color: Color(0xFF303030))),
-      subtitle: Text(widget.mediaxObject.location,
-          style: TextStyle(
-              fontFamily: "Trajan Pro",
-              height: 1.0,
-              fontSize: 8,
-              fontWeight: FontWeight.bold,
-              color: Color(0xFF303030))),
-      //trailing: Text(widget.mediaxObject.timestamp),
-      onTap: () {
-        //When user clicks the row/tile they go to the song's detail page
+    return Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: PhysicalModel(
+            color: Colors.white,
+            elevation: 18,
+            shadowColor: Colors.black,
+            borderRadius: BorderRadius.circular(20),
+            child: ListTile(
+              tileColor: Colors.white,
+              title: Text(widget.mediaxObject.filename,
+                  style: TextStyle(
+                      fontFamily: "Trajan Pro",
+                      height: 1.0,
+                      fontSize: 10,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xFF303030))),
+              subtitle: Text(widget.mediaxObject.location,
+                  style: TextStyle(
+                      fontFamily: "Trajan Pro",
+                      height: 1.0,
+                      fontSize: 8,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xFF303030))),
+              //trailing: Text(widget.mediaxObject.timestamp),
+              onTap: () {
+                //When user clicks the row/tile they go to the song's detail page
 
-        Navigator.push(
-            context,
-            new MaterialPageRoute(
-                builder: (context) => MediaxDetailPage(widget.mediaxObject)));
-      },
-    );
+                Navigator.push(
+                    context,
+                    new MaterialPageRoute(
+                        builder: (context) =>
+                            MediaxDetailPage(widget.mediaxObject)));
+              },
+            )));
   }
 }
