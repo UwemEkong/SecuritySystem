e5bd96aea751364d40398f3b27f78ad346735c05
Niraj Patel <niraj321@yahoo.com>
2022-05-08 17:02:52 -0500

    added media population file, slightly improved look of FAQ page...

---
+++ dbShite/mediaxpopulation.sql
@@ -0,0 +1,40 @@
+INSERT into BigBroSecurity.mediax
+values (76, 'Image1', 0, 0, 'In Lisle, IL', 'image_1Mon Apr 25 15_51_50 2022', '4/01/2022, 2:11:15 PM', 3, 1, 1,
+        'Shadow on a Wall', 'Crime', 101, 1);
+
+INSERT into BigBroSecurity.mediax
+values (77, 'Image2', 0, 0, 'In Lisle, IL', 'image_1Mon Apr 25 15_52_58 2022', '4/03/2022, 4:22:20 PM', 3, 0, 0,
+        'Image2', '1', 101, 1);
+
+INSERT into BigBroSecurity.mediax
+values (78, 'Video3', 0, 1, 'In Lisle, IL', 'image_1Mon Apr 25 15_53_04 2022', '4/05/2022, 6:33:25 PM', 3, 1, 1,
+        'Logitech Speaker', 'Safety', 101, 1);
+
+INSERT into BigBroSecurity.mediax
+values (79, 'Video4', 0, 1, 'In Lisle, IL', 'image_1Mon Apr 25 15_53_13 2022', '4/07/2022, 8:44:30 PM', 3, 0, 0,
+        'Image4', '1', 101, 1);
+
+INSERT into BigBroSecurity.mediax
+values (80, 'Image5', 0, 0, 'In Lisle, IL', 'image_1Mon Apr 25 15_53_22 2022', '4/09/2022, 10:55:35 PM', 3, 1, 1,
+        'Wireless Phone Charger', 'Environmental', 101, 1);
+
+INSERT into BigBroSecurity.mediax
+values (81, 'Image6', 0, 0, 'In Lisle, IL', 'image_1Mon Apr 25 15_53_38 2022', '4/11/2022, 12:11:40 PM', 3, 0, 0,
+        'Image6', '1', 101, 1);
+
+INSERT into BigBroSecurity.mediax
+values (82, 'Image7', 0, 0, 'In Lisle, IL', 'image_1Mon Apr 25 15_53_53 2022', '4/13/2022, 2:22:45 PM', 3, 0, 1,
+        'Colorful Keyboard', 'Environmental', 101, 1);
+
+INSERT into BigBroSecurity.mediax
+values (83, 'Image8', 0, 0, 'In Lisle, IL', 'image_1Mon Apr 25 15_54_00 2022', '4/15/2022, 4:33:50 PM', 3, 1, 0,
+        'Image8', '1', 101, 1);
+
+INSERT into BigBroSecurity.mediax
+values (84, 'Image9', 0, 0, 'In Lisle, IL', 'image_1Mon Apr 25 15_54_14 2022', '4/17/2022, 6:44:55 PM', 2, 0, 0,
+        'Image9', '1', 101, 1);
+
+INSERT into BigBroSecurity.mediax
+values (85, 'Image10', 0, 0, 'In Lisle, IL', 'image_1Mon Apr 25 15_54_41 2022', '4/19/2022, 8:55:10 PM', 2, 0, 1,
+        'Computer Screen', 'Community', 101, 1);
+
